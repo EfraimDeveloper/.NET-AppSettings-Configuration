a8565cb35f4a58b49a0794a224fea2dd1cc75682
Efraim Marcelino <efraimprogrm@gmail.com>
2026-09-07 13:28:54 +0100

    add loggin

--- Configuration/Service.cs
+++ Configuration/Service.cs
@@ -6,14 +6,17 @@ namespace Configuration
     {
 
         private readonly Appsetings _appsetings;
+        private readonly ILogger<Service> _logger;
 
-        public Service(Appsetings appsetings) {
+        public Service(IOptions<Appsetings> appsetings, ILogger<Service> logger) {
 
-            _appsetings = appsetings;
+            _appsetings = appsetings.Value;
+            _logger = logger;
         }
 
         public void ShowAppsetings()
         {
+            _logger.LogInformation("Showing AppSettings:");
             Console.WriteLine($"NameApp: {_appsetings.NameApp}");
             Console.WriteLine($"Version: {_appsetings.Version}");
             Console.WriteLine($"MaxNumberOfAttempts: {_appsetings.MaxNumberOfAttempts}");
